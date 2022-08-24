cbf411cf35767704e247d65833a34faaeb958210
shahbaz Mahmood <shahbazmahmooood@gmail.com>
2022-08-24 17:36:34 -0400

    comment

--- ARPlanets/ViewModels/PlanetCollectionViewModel.swift
+++ ARPlanets/ViewModels/PlanetCollectionViewModel.swift
@@ -14,6 +14,8 @@ class PlanetCollectionViewModel: ObservableObject{
     
     private let userDefaults = UserDefaults.standard
     
+    
+    //this is just ne testing setting the planet to see if its possible.
     func setPlanet(){
         var test = userDefaults.object(forKey: "currentPlanetSelection")
         userDefaults.set("Sun", forKey: "currentPlanetSelection")
