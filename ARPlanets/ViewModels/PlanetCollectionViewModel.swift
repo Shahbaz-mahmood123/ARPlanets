53abee02d3921903be7750912b998c133956c553
shahbaz Mahmood <shahbazmahmooood@gmail.com>
2022-09-05 17:18:12 -0400

    Buggy higlighting for collection view

--- ARPlanets/ViewModels/PlanetCollectionViewModel.swift
+++ ARPlanets/ViewModels/PlanetCollectionViewModel.swift
@@ -13,6 +13,7 @@ class PlanetCollectionViewModel: ObservableObject{
     
     @Published var planet = Planets()
     @Published var currentPlanetSelection: String?
+    @Published var cellcolor : Color? 
     @Published var arrayOfPlanets: [PlanetList] = [
         PlanetList(name: "moon"),
         PlanetList(name: "Earth Daytime"),
@@ -29,17 +30,18 @@ class PlanetCollectionViewModel: ObservableObject{
     
     private let userDefaults = UserDefaults.standard
     
+    
     //this is just ne testing setting the planet to see if its possible.
     func setPlanet(_ selectedPlanet: String){
-        userDefaults.set(selectedPlanet, forKey: "currentPlanetSelection")
+        userDefaults.set(selectedPlanet, forKey: UserDefaultsConstants.currentPlanetSelection)
         print(userDefaults.object(forKey: UserDefaultsConstants.currentPlanetSelection))
     }
-    
-    func setColorForCell(_ isHighlighted: Bool) -> Color{
+    //Probably should use userdefaults here to set the current planet as highlighted. 
+    func setColorForCell(_ isHighlighted: Bool){
         if isHighlighted == true {
-            return Color.red
+            cellcolor =  Color.red
         } else {
-            return Color.cyan
+            cellcolor =  Color.cyan
         }
     }
 }
--- ARPlanets/Views/PlanetCollectionViewCellUI.swift
+++ ARPlanets/Views/PlanetCollectionViewCellUI.swift
@@ -11,13 +11,13 @@ struct PlanetCollectionViewCellUI: View {
     @State var name: String = ""
     @ObservedObject var vm = PlanetCollectionViewModel()
     @State var isHiglighted = false
-    @State var currentColor = Color.cyan
+    //@State var currentColor = Color.cyan
     private let width = (UIScreen.main.bounds.width/3)-20
     
     var body: some View {
         ZStack{
         RoundedRectangle(cornerRadius: 8)
-                .frame(width: 220, height: 200).foregroundColor(currentColor)
+                .frame(width: 220, height: 200).foregroundColor(vm.cellcolor ?? Color.cyan)
             Text("\(name)").font(.largeTitle).foregroundColor(.white)
         }.onTapGesture {
             vm.setPlanet(name)
